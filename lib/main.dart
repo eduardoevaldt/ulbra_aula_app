785962fcf22634bce8332e3edfce9b3ea701a9f1
eduardoevaldt <eduardo.webber@rede.ulbra.br>
2022-04-26 19:19:09 -0300

    Criado a Pagina_Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pagina_inicial.dart';
 
 void main() {
   runApp(const MyApp());
@@ -58,9 +59,102 @@ class _MyHomePageState extends State<MyHomePage> {
                 alignment: Alignment.topCenter,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [Padding(padding: EdgeInsets.only(top: 50))],
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(top: 50),
+                    ),
+                    Text(
+                      "Olá! ",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Bem vindo.",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+                    ),
+                  ],
                 ),
               ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Digite seu login para prosseguir. ",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              const Divider(
+                thickness: 0.2,
+                color: Colors.grey,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 5),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              Align(
+                alignment: Alignment.topLeft,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    suffixIcon:
+                        Icon(Icons.person_outline, color: Colors.grey[400]),
+                    focusColor: Colors.grey[300],
+                    labelText: "Digite seu email @rede",
+                    labelStyle: const TextStyle(fontSize: 12),
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              SizedBox(
+                width: 400,
+                height: 70,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: ElevatedButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Proximo",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PaginaInicial()));
+                    },
+                  ),
+                ),
+              )
             ],
           ),
         ],
---
+++ lib/pagina_inicial.dart
@@ -0,0 +1,363 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_new
+
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+
+class PaginaInicial extends StatefulWidget {
+  const PaginaInicial({Key? key}) : super(key: key);
+
+  @override
+  PaginaInicialState createState() {
+    return PaginaInicialState();
+  }
+}
+
+class PaginaInicialState extends State<PaginaInicial> {
+  String dropdownvalue = '2022/1';
+  var itens = ['2022/1', '2021/2', '2021/1', '2020/2'];
+  Widget _salasVirtuais() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: ListView(
+        children: [
+          Text(
+            'Salas Virtuais',
+            style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text('Período:'),
+          SizedBox(
+            height: 10,
+          ),
+          DropdownButtonFormField(
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            value: dropdownvalue,
+            items: itens.map((String itens) {
+              return DropdownMenuItem(
+                value: itens,
+                child: Text(itens),
+              );
+            }).toList(),
+            onChanged: (value) {
+              setState(() {});
+            },
+          ),
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 15)),
+              Container(
+                height: 120,
+                width: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Color.fromARGB(255, 164, 34, 25),
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 30, left: 20),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 230,
+                            child: Text(
+                              'Desenvolvimento de Sistemas Móveis',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 16)),
+                          SizedBox(
+                              width: 230,
+                              child: Text('Turma: 0077-A - 31N - Graduação',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                  ))),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: CircularPercentIndicator(
+                        radius: 65.0,
+                        lineWidth: 5.0,
+                        percent: 0.35,
+                        center: new Text(
+                          '35%',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        progressColor: Colors.yellow,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8)),
+                color: Colors.white),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 10)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(
+                        Icons.star,
+                        color: Colors.grey[350],
+                      ),
+                      Icon(
+                        Icons.forum_rounded,
+                        color: Colors.grey[350],
+                      ),
+                      Icon(
+                        Icons.error_rounded,
+                        color: Colors.grey[350],
+                      ),
+                    ]),
+                Padding(padding: EdgeInsets.only(top: 10)),
+                SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                      ),
+                      onPressed: () {},
+                      child: Text('Entrar na sala')),
+                )
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 15)),
+              Container(
+                height: 120,
+                width: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Color.fromARGB(255, 25, 54, 26),
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 30, left: 20),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 230,
+                            child: Text(
+                              'Desenvolvimento de Aplicações Orientada a Componenetes',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 16)),
+                          SizedBox(
+                              width: 230,
+                              child: Text('Turma: 0232-A - 51N - Graduação',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                  ))),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: CircularPercentIndicator(
+                        radius: 65.0,
+                        lineWidth: 5.0,
+                        percent: 0.64,
+                        center: new Text(
+                          '64%',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        progressColor: Colors.yellow,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8)),
+                color: Colors.white),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 10)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(
+                        Icons.star,
+                        color: Colors.grey[350],
+                      ),
+                      Icon(
+                        Icons.forum_rounded,
+                        color: Colors.grey[350],
+                      ),
+                      Icon(
+                        Icons.error_rounded,
+                        color: Colors.grey[350],
+                      ),
+                    ]),
+                Padding(padding: EdgeInsets.only(top: 10)),
+                SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                      ),
+                      onPressed: () {},
+                      child: Text('Entrar na sala')),
+                )
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 15)),
+              Container(
+                height: 120,
+                width: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.cyan,
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 30, left: 20),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 230,
+                            child: Text(
+                              'Cultura Religiosa',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 16)),
+                          SizedBox(
+                              width: 230,
+                              child: Text('Turma: 80414 - EAD - Graduação',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                  ))),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: CircularPercentIndicator(
+                        radius: 65.0,
+                        lineWidth: 5.0,
+                        percent: 0.87,
+                        center: new Text(
+                          '87%',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        progressColor: Colors.yellow,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    bottomRight: Radius.circular(8)),
+                color: Colors.white),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 10)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(
+                        Icons.star,
+                        color: Colors.grey[350],
+                      ),
+                      Icon(
+                        Icons.forum_rounded,
+                        color: Colors.grey[350],
+                      ),
+                      Icon(
+                        Icons.error_rounded,
+                        color: Colors.grey[350],
+                      ),
+                    ]),
+                Padding(padding: EdgeInsets.only(top: 10)),
+                SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                      ),
+                      onPressed: () {},
+                      child: Text('Entrar na sala')),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60.0),
+          child: AppBar(
+            backgroundColor: Colors.white,
+            title: Row(
+              children: [
+                Image.asset(
+                  'assets/images/aula.png',
+                  height: 55,
+                ),
+              ],
+            ),
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.black87,
+          ),
+        ),
+        body: _salasVirtuais(),
+        backgroundColor: Colors.grey[200],
+      ),
+    );
+  }
+}
